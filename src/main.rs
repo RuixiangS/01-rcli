630506bf93f55fdf887a0bf4459319e7705081d4
RuixiangS <ruixiang.shen@outlook.com>
2024-12-17 00:40:30 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, rust!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn empty_test() {
+        // 这是一个空的测试，确保 cargo test 不会失败
+    }
 }
